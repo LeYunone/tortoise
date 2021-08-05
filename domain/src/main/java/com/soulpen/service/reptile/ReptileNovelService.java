90b9bd086b03e92f31bfb6af6672a8220f2efd91
Leyuna <365627310@qq.com>
2021-08-05 14:26:39 +0800

    [添加]

---
+++ domain/src/main/java/com/soulpen/service/reptile/ReptileNovelService.java
@@ -0,0 +1,12 @@
+package com.soulpen.service.reptile;
+
+/**
+ * @author pengli
+ * @create 2021-08-05 14:24
+ *
+ *  爬小说核心服务
+ */
+public class ReptileNovelService {
+
+
+}
---
+++ domain/src/main/java/com/soulpen/util/ReptileUtils.java
@@ -0,0 +1,8 @@
+package com.soulpen.util;
+
+/**
+ * @author pengli
+ * @create 2021-08-05 14:26
+ */
+public class ReptileUtils {
+}
