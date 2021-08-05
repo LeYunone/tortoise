b1f67503dd073f3c229afb576ad9fc5c950699ee
Leyuna <365627310@qq.com>
2021-08-05 13:38:41 +0800

    [项目搭建]

--- app/src/main/java/com/soulpen/control/UserControl.java
+++ app/src/main/java/com/soulpen/control/UserControl.java
@@ -1,8 +1,11 @@
 package com.soulpen.control;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author pengli
  * @create 2021-08-05 09:30
  */
+@RequestMapping()
 public class UserControl {
 }
---
+++ domain/src/main/java/com/soulpen/service/user/UserService.java
@@ -0,0 +1,8 @@
+package com.soulpen.service.user;
+
+/**
+ * @author pengli
+ * @create 2021-08-05 13:33
+ */
+public class UserService {
+}
